c39c1a4aaf409fac4d33fc50a8f6f2b84e070162
steely-glint <thp@westhawk.co.uk>
2018-08-05 15:50:26 +0100

    Added episode 5 and fixed some stuff

--- makeEp.sh
+++ makeEp.sh
@@ -8,9 +8,9 @@ echo "<date>" `date -R -j -r docs/episodes/$1/*.mp3 `"</date>"
 echo "<description>Text here please</description>"
 echo "<tags>tags,here,please</tags>"
 # estimated duration: 1560.408000 sec
-afinfo docs/episodes/$1/*.mp3 | awk '/^ estimated duration:/ { print "<duration>" int($3) "</duration"}'
+afinfo docs/episodes/$1/*.mp3 | awk '/^estimated duration:/ { print "<duration>" int($3) "</duration>"}'
 echo "<title>Title here</title>"
 echo "<guest>Text here please</guest>"
-echo "<transcription></transcription>"
+echo "<transcription>DistributedFuture$1.rtf</transcription>"
 echo "<links><link></link></links>"
 echo "</episode>"
