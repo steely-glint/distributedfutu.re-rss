66672689bfccb94491b551b05ee49a1a880c0223
steely-glint <thp@westhawk.co.uk>
2019-01-20 13:06:44 +0000

    Added SDR

--- gen.sh
+++ gen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-MAXEP=15
+MAXEP=16
 xsltproc --nonet --stringparam maxEpisode ${MAXEP} --stringparam current-date "`date -R`" -o docs/rss/DistributedFuture.rss  src/mkrss.xsl src/headpart-rss.xml 
 xsltproc --stringparam maxEpisode ${MAXEP} --stringparam current-date "`date -R`" -o docs/index.html  src/mkindex.xsl src/headpart-rss.xml 
 
