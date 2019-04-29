52703b18311675844b8f059f66d2adfbbfb0fffd
steely-glint <thp@westhawk.co.uk>
2019-04-29 23:21:38 +0200

    and push 23

--- gen.sh
+++ gen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-MAXEP=22
+MAXEP=23
 xsltproc --nonet --stringparam maxEpisode ${MAXEP} --stringparam current-date "`date -R`" -o docs/rss/DistributedFuture.rss  src/mkrss.xsl src/headpart-rss.xml 
 xsltproc --stringparam maxEpisode ${MAXEP} --stringparam current-date "`date -R`" -o docs/index.html  src/mkindex.xsl src/headpart-rss.xml 
 
