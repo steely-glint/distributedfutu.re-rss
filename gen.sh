24bd57135e44643c2c7e8f6a405cc73294854472
Tim Panton <tim@pi.pe>
2022-06-18 12:45:30 +0100

    Added 75 - no links yet....

--- gen.sh
+++ gen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-MAXEP=74
+MAXEP=75
 xsltproc --nonet --stringparam maxEpisode ${MAXEP} --stringparam current-date "`date -R`" -o docs/rss/DistributedFuture.rss  src/mkrss.xsl src/headpart-rss.xml 
 xsltproc --stringparam maxEpisode ${MAXEP} --stringparam current-date "`date -R`" -o docs/index.html  src/mkindex.xsl src/headpart-rss.xml 
 xsltproc --stringparam maxEpisode ${MAXEP} --stringparam current-date "`date +%Y-%m-%d`" -o docs/sitemap.xml src/mksitemap.xsl src/headpart-rss.xml 
